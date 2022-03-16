e2b51572646dbe94677fe08a36f2dfaf90e5034c
lhm20 <lhm20@qq.com>
2022-03-16 11:24:47 +0800

    second commit

--- src/main/java/com/ygh/GitTest.java
+++ src/main/java/com/ygh/GitTest.java
@@ -2,6 +2,7 @@ package com.ygh;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!!!");
+        System.out.println("hello git1!!!");
+        System.out.println("hello git2!!!");
     }
 }
